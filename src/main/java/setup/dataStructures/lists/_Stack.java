c1136daec42ac291c1bbab2275c3ce6e2b0e8125
Rodrigo Bertini <rodrigo.bertini@gmail.com>
2023-08-17 16:41:23 +0100

    Inserting Stack and tests

---
+++ src/main/java/setup/dataStructures/lists/_Stack.java
@@ -0,0 +1,69 @@
+package setup.dataStructures.lists;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class _Stack {
+
+    /**
+     *  Details:
+     *  -   LIFO -> Last In First Out
+     *
+     *  -   The "best" way to reproduce a Stack is using a LinkedList,
+     *      because from the beginning always is O(1) to Push AND Remove items.
+     *      And it's great for a LIFO
+     *
+     */
+
+    private Node top;
+    private int height;
+
+    public _Stack(int value){
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node tmp = top;
+        while(tmp != null){
+            System.out.println(tmp.value);
+            tmp = tmp.next;
+        }
+    }
+
+    public void push(int i) {
+        Node newNode = new Node(i);
+        if(height > 0) newNode.next = top;
+        top = newNode;
+        height++;
+    }
+
+    public Node pop() {
+        if(height == 0) return null;
+
+        Node tmp = top;
+        top = top.next;
+        tmp.next = null;
+        height--;
+        return tmp;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class Node {
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/setup/dataStructures/stack/_StackTest.java
@@ -0,0 +1,59 @@
+package setup.dataStructures.stack;
+
+import org.junit.jupiter.api.Test;
+import setup.dataStructures.lists._Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _StackTest {
+
+    @Test
+    void validateMyStack(){
+        _Stack stack = new _Stack(4);
+        assertEquals(4, stack.getTop().getValue());
+        assertEquals(1, stack.getHeight());
+        stack.printStack();
+    }
+
+    @Test
+    void validateMyPush(){
+        _Stack stack = new _Stack(3);
+        assertEquals(3, stack.getTop().getValue());
+        assertEquals(1, stack.getHeight());
+
+        stack.push(2);
+        assertEquals(2, stack.getTop().getValue());
+        assertEquals(2, stack.getHeight());
+
+        stack.push(1);
+        assertEquals(1, stack.getTop().getValue());
+        assertEquals(3, stack.getHeight());
+    }
+
+    @Test
+    void validateMyPop() {
+        _Stack stack = new _Stack(3);
+        assertEquals(3, stack.getTop().getValue());
+        assertEquals(1, stack.getHeight());
+
+        stack.push(2);
+        assertEquals(2, stack.getTop().getValue());
+        assertEquals(2, stack.getHeight());
+
+        stack.push(1);
+        assertEquals(1, stack.getTop().getValue());
+        assertEquals(3, stack.getHeight());
+
+        stack.pop();
+        assertEquals(2, stack.getTop().getValue());
+        assertEquals(2, stack.getHeight());
+
+        stack.pop();
+        assertEquals(3, stack.getTop().getValue());
+        assertEquals(1, stack.getHeight());
+
+        stack.pop();
+        assertNull(stack.getTop());
+        assertEquals(0, stack.getHeight());
+    }
+}
