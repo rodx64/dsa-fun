43ef8c03037d28a6648af071f1179d5bda780349
rodrigo bertini <rodrigo.bertini@gmail.com>
2024-01-07 15:53:08 +0000

    HashTable implemented
    Need to do some benchmark

---
+++ src/test/java/setup/dataStructures/hash_tables/_HashTableTest.java
@@ -0,0 +1,54 @@
+package setup.dataStructures.hash_tables;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class _HashTableTest {
+
+    @Test
+    void validateMyHashTable(){
+        _HashTable hashTable = new _HashTable();
+        hashTable.printTable();
+    }
+
+    @Test
+    void givenHashMap_whenSetValues_thenPrint(){
+        _HashTable hashTable = new _HashTable();
+        hashTable.set("nails", 100);
+        hashTable.set("tile", 40);
+        hashTable.set("lumber", 500);
+        hashTable.set("bolts", 300);
+        hashTable.set("screws", 300);
+
+        hashTable.printTable();
+    }
+
+    @Test
+    void givenHashMap_whenGetTheValue_thenValidateValueIsTrue(){
+        _HashTable hashTable = new _HashTable();
+        hashTable.set("nails", 100);
+        hashTable.set("tile", 40);
+        hashTable.set("lumber", 500);
+        hashTable.set("bolts", 300);
+        hashTable.set("screws", 300);
+        hashTable.printTable();
+
+        int value = hashTable.get("lumber");
+        assertEquals(500, value);
+    }
+
+    @Test
+    void givenHashMap_whenGetTheKey_thenValidateKeyIsTrue(){
+        _HashTable hashTable = new _HashTable();
+        hashTable.set("nails", 100);
+        hashTable.set("tile", 40);
+        hashTable.set("lumber", 500);
+        hashTable.set("bolts", 300);
+        hashTable.set("screws", 300);
+        hashTable.printTable();
+
+        String firstKey = (String) hashTable.keys().get(0);
+        assertEquals("screws", firstKey);
+    }
+}
