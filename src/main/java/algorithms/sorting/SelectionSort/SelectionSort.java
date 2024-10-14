f5a5ed8f2eae160701f14c84e6ab8504ccad0c98
rodx64 <rodrigo.bertini@gmail.com>
2024-10-14 21:00:41 +0100

    refactoring selection sort

--- src/main/java/algorithms/sorting/SelectionSort/SelectionSort.java
+++ src/main/java/algorithms/sorting/SelectionSort/SelectionSort.java
@@ -4,18 +4,30 @@ public class SelectionSort {
 
     public static void run(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            if (i != minIndex) {
+            int minIndex = findIndexOfMinimumValue(array, i);
+            boolean isSmallerThanCurrent = i != minIndex;
+
+            if (isSmallerThanCurrent) {
+                // swap current with smaller
                 int tmp = array[i];
                 array[i] = array[minIndex];
                 array[minIndex] = tmp;
             }
         }
+    }
+
+    private static int findIndexOfMinimumValue(int[] array, int i) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < array.length; j++) {
+            int currentValue = array[minIndex];
+            int nextValue = array[j];
+
+            if (nextValue < currentValue) {
+                minIndex = j;
+            }
+        }
+        return minIndex;
     }
 
 }
